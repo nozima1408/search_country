b8cf1740a90e65b5a9efc39676451a76d5eac536
nozima1408 <nozimag008@gmail.com>
2026-03-12 17:48:28 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ let countries = [];
 // Fetch countries
 async function getData() {
   try {
-    let res = await fetch("https://restcountries.com/v3.1/all?fields=name");
+    let res = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flags",
+    );
     countries = await res.json();
 
     renderCountries(countries); // show all countries first
@@ -26,9 +28,18 @@ getData();
 function renderCountries(data) {
   divBox.innerHTML = "";
 
+  if (data.length === 0) {
+    let p = document.createElement("p");
+    p.textContent = "No matching countries";
+    p.className = "text-2xl text-[#2B3844] dark:text-white font-semibold mt-16";
+    divBox.appendChild(p);
+    return;
+  }
+
   data.forEach((countryInfo) => {
     divBox.innerHTML += `
-      <div class="bg-white dark:bg-[#2B3844] shadow-md hover:shadow-xl transition-all duration-300 border border-[#E5E7EB] dark:border-0 w-[300px] px-4 text-center py-10 rounded-md">
+      <div class="bg-white dark:bg-[#2B3844] shadow-md hover:shadow-xl transition-all duration-300 border border-[#E5E7EB] dark:border-0 w-[300px] text-center pb-5 rounded-md">
+        <img src=${countryInfo.flags.png} class="w-[300px] h-[200px] object-cover pb-5" />
         <p class="text-[#1F2937] dark:text-white text-[18px] font-semibold">
           ${countryInfo.name.common}
         </p>
